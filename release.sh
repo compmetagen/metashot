456a9afadcef67c843e2bf3590b34ace2529d804
Davide Albanese <davide.albanese@gmail.com>
2020-05-16 21:32:57 +0200

    Fix release.sh

--- release.sh
+++ release.sh
@@ -14,8 +14,8 @@ for path in dockerfiles/*; do
     
     docker build -t $REGISTRY/$toolname:latest $path
     docker tag $REGISTRY/$toolname:latest $REGISTRY/$toolname:$version
-    docker push $USERNAME/$toolname:latest
+    docker push $REGISTRY/$toolname:latest
     docker push $REGISTRY/$toolname:$version
-    docker rmi $USERNAME/$toolname:latest
-    docker rmi $USERNAME/$toolname:$version
+    docker rmi $REGISTRY/$toolname:latest
+    docker rmi $REGISTRY/$toolname:$version
 done
